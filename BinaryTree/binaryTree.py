9e83c4698602f1b95a9c86b60524299bf1236418
attilagyen <attila.gyen@appercell.com>
2020-12-04 08:53:02 +0100

    binary tree raw implementation

---
+++ BinaryTree/binaryTree.py
@@ -0,0 +1,12 @@
+class Node:
+  def __init__(self,key):
+    self.left = None
+    self.right = None
+    self.val = key
+
+
+if __name__ == '__main__':
+  root = Node(1)
+  root.left = Node(2)
+  root.right = Node(3)
+  root.left.left = Node(4)
